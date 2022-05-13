a059eb061591dcce7fa8623b64db12fd47b487c7
rajni <sainirajni292000@gmail.com>
2022-05-13 14:31:15 +0530

    backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const apiroute = require('./routes/auth')
+const cors = require('cors')
+const dotenv = require('dotenv')
+// Initialize the application
+const app = express()
+
+// setup server port
+const port = 8080
+
+app.get("/", (req,res) =>{
+    res.send("running app")
+})
+
+app.use(express.json(),cors())
+
+
+// send message for your localhost
+app.use('/api/users', apiroute)
+
+dotenv.config()
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser:true} )
+.then(() =>{
+    console.log('Database Connected')
+}).catch(err => console.log(err))
+
+
+// Launch the admin dashbord system  backend app
+app.listen(port, () =>{
+    console.log(`running app on port: 'http://localhost:${port}/`)
+})
---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const { request } = require('express')
+const Controller = require('../controller/User')
+const verify = require('./authVerify')
+const cors = require('cors');
+
+router.post('/register', cors(),Controller.signUp)
+
+router.post('/login', cors(),Controller.logIn)
+
+router.get('/get_all', cors(),verify, Controller.getAllUsers)
+module.exports = router
